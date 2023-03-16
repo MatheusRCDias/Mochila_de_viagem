b71d2d9c582727fb47a4ec23eb365441ff8de21f
MatheusRCDias <114764338+MatheusRCDias@users.noreply.github.com>
2023-03-16 20:45:02 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,76 @@
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("item")) || []
+console.log(localStorage.getItem("item"))
+
+
+itens.forEach( (elemento)=>{
+    criaElemento(elemento)
+})
+
+form.addEventListener("submit", (evento) => {
+    
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+    const existe = itens.find( elemento => elemento.nome===nome.value)
+    
+    
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    if(existe) {
+        itemAtual.id = existe.id
+        atualizaElemento(itemAtual)
+        itens[existe.id] = itemAtual
+
+    } else{
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id +1 : 0;
+        criaElemento(itemAtual)
+        itens.push(itemAtual)        
+    }
+
+    localStorage.setItem("item", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
+    
+})
+
+function criaElemento(item) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDeleta(item.id))
+
+    lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item) {
+    document.querySelector( " [data-id='" +item.id+ "' ] " )
+} 
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "x"
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id){
+    tag.remove(tag)
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+    localStorage.setItem("item", JSON.stringify(itens))
+    console.log(itens)
+}
